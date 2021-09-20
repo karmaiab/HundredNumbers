de972521ddc697c83c29ee788f3785ab97db2831
unknown <supergamer9012@gmail.com>
2021-09-20 20:54:19 +0300

    adding a int

---
+++ src/hundrednumbers/HundredNumbers.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hundrednumbers;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Danek
+ */
+public class HundredNumbers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int number;
+        Scanner scanner = new Scanner(System.in);
+    }
+    
+}
